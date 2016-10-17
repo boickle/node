384d998902de9e1f0f2ed3d5f66e199bbb78e81b
Brian Oickle <boickle@bigroomstudios.com>
2016-10-17 13:04:04 -0400

    Changed some date formattig

--- models/outtemp.js
+++ models/outtemp.js
@@ -3,7 +3,7 @@ var connection = require('../connection');
 function outTemp() {
   this.get = function(res) {
     connection.acquire(function(err, con) {
-      con.query('select dateTime, outTemp from archive order by dateTime desc limit 1', function(err, result) {
+      con.query('select from_unixtime(dateTime) outTemp from archive order by dateTime desc limit 1', function(err, result) {
         con.release();
         res.send(result);
       });
@@ -11,7 +11,7 @@ function outTemp() {
   };
   this.getByDate = function(date, res) {
     connection.acquire(function(err, con) {
-      con.query('select dateTime, outTemp from archive where dateTime > ?', [date], function(err, result) {
+      con.query('select from_unixtime(dateTime), outTemp from archive where from_unixtime(dateTime) > ?', [date], function(err, result) {
         con.release();
         res.send(result);
       });
@@ -19,7 +19,7 @@ function outTemp() {
   };
   this.getBetweenDate = function(startDate, endDate, res) {
     connection.acquire(function(err, con) {
-      con.query('select dateTime, outTemp from archive where dateTime BETWEEN ? and ?', [startDate,endDate], function(err, result) {
+      con.query('select from_unixtime(dateTime), outTemp from archive where from_unixtime(dateTime) BETWEEN ? and ?', [startDate,endDate], function(err, result) {
         con.release();
         res.send(result);
       });
